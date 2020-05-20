9010bb6e1b5a38c2c1b580a407435c43df2acb40
SharpChan <chengfeng3539@126.com>
2020-05-20 15:56:59 +0800

    删除pom中多余依赖

--- src/main/java/com/example/demo/enums/Constants.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.enums;
-
-public  final class Constants {
-
-}
--- src/main/java/com/example/demo/intf/TableParseIntf.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.intf;
-
-import com.example.demo.entity.RowEntity;
-
-import java.util.List;
-
-public interface TableParseIntf {
-    List<RowEntity> parseTable(String table);
-}
